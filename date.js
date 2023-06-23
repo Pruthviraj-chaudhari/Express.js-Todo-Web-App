1cbc61b85efcafbb619734a089e0e5b821328ae4
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-06-23 12:38:34 +0530

    Convert to Arrow functions

--- date.js
+++ date.js
@@ -1,11 +1,44 @@
-module.exports.getDate = function() {
+// module.exports.getDate = function() {
 
+//     let currentDate = new Date();
+//     let options = {
+//         weekday: "long",
+//         day: "numeric",
+//         month: "long",
+//         // year: "numeric"
+//     }
+//     return currentDate.toLocaleDateString("en-US", options);
+// }
+
+// OR
+
+// Arrow Function
+exports.getDate = () => {
     let currentDate = new Date();
     let options = {
         weekday: "long",
         day: "numeric",
         month: "long",
-        // year: "numeric"
+    }
+    return currentDate.toLocaleDateString("en-US", options);
+}
+
+
+// Another Function
+exports.getDay = function() {
+    let currentDate = new Date();
+    let options = {
+        weekday: "long",
+    }
+    return currentDate.toLocaleDateString("en-US", options);
+}
+
+// Another Function
+exports.getYear = function() {
+    let currentDate = new Date();
+    let options = {
+        month: "long",
+        year: "numeric"
     }
     return currentDate.toLocaleDateString("en-US", options);
 }
